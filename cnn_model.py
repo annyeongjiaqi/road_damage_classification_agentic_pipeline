c63b01132e5319c43fed247cf7c445f8c181657a
jiaqi <annyeongjiaqi@gmail.com>
2025-07-22 01:45:34 +0800

    Added cnn_model.py

---
+++ cnn_model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+MODEL_PATH = "model/damage_classifier.h5"
+model = tf.keras.models.load_model(MODEL_PATH)
+classes = ['D00', 'D10', 'D20', 'D40']
+
+def predict_damage(image: Image.Image):
+    img = image.resize((224, 224))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    pred = model.predict(img)[0]
+    return dict(zip(classes, pred))
